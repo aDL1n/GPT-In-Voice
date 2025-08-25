d1e12c18a89312284061aa5ab2e689b5f06678ec
aDL1n <111649965+a1n@users.noreply.github.com>
2025-08-25 19:08:44 +0300

    oh fuck

--- src/main/java/dev/adlin/Main.java
+++ src/main/java/dev/adlin/Main.java
@@ -4,6 +4,6 @@ package dev.adlin;
 public class Main {
 
     public static void main(String[] args) {
-        Bot bot = new Bot("MTMxNzE0NzQ4OTAyNzEwMDcwMg.G05YQf.eQcGj-TTDMFQfG-ZqegBM5WHBa4cUQ_rJRrR38");
+        Bot bot = new Bot("token");
     }
 }
