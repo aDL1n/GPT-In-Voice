d703e7ab949ec99672c17addb7278ffc44a7f86e
aDL1n <111649965+a1n@users.noreply.github.com>
2025-09-14 13:22:10 +0300

    Added join and leave voice listener callback

--- src/main/java/dev/adlin/listeners/DiscordVoiceListener.java
+++ src/main/java/dev/adlin/listeners/DiscordVoiceListener.java
@@ -1,25 +1,21 @@
 package dev.adlin.listeners;
 
-import net.dv8tion.jda.api.entities.channel.unions.AudioChannelUnion;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.function.Consumer;
+
 public class DiscordVoiceListener extends ListenerAdapter {
+
+    private final Consumer<GuildVoiceUpdateEvent> consumer;
+
+    public DiscordVoiceListener(Consumer<GuildVoiceUpdateEvent> consumer) {
+        this.consumer = consumer;
+    }
 
     @Override
     public void onGuildVoiceUpdate(@NotNull GuildVoiceUpdateEvent event) {
-        AudioChannelUnion joinedChannel = event.getChannelJoined();
-        AudioChannelUnion leftChannel = event.getChannelLeft();
-
-        if (joinedChannel != null) {
-//            event.getEntity();
-        }
-        if (leftChannel != null) {
-
-        }
-        if (joinedChannel != null && leftChannel != null) {
-
-        }
+        consumer.accept(event);
     }
 }
